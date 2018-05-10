18b7865cd500f87a91033f41f419712e21de9200
Lee Ikard <Leeeyecard@gmail.com>
2018-05-09 19:06:19 -0600

    Update checkdisk.sh

--- checkdisk.sh
+++ checkdisk.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
-sudo apt update
-sudo apt install hdparm -y
+echo "run as root"
+sleep 1
+ apt update
+ apt install hdparm pm-utils -y
 printf "Disk name (eg /dev/sdb1): "
 read disk 
-sudo umount $disk
+ umount $disk
 set -e
 echo ""
 echo "Checking Disk $disk"
-#sudo umount $disk
+# umount $disk
 workplace="/tmp/diskcheck-$$"
 workfile="DT-$$"
 
 echo "Working in $workplace"
-sudo mkdir $workplace
-sudo mount $disk $workplace/
-
-echo "Write test: Cache On"
+ mkdir $workplace
+ mount $disk -o defaults,noatime $workplace/
+echo "Test: Cache On"
 hdparm -W1 $disk
-dd if=/dev/zero of=$workplace/$workfile bs=1G count=1 oflag=direct    
-rm -r $workplace/$workfile
-echo "Write test: Cache Off"
-hdparm -W0 $disk
-dd if=/dev/zero of=$workplace/$workfile bs=1G count=1 oflag=direct    
-rm -r $workplace/$workfile
+ hdparm -Tt $disk
 
-echo "Read test: Cache On"
-hdparm -W1 $disk
-sudo hdparm -Tt $disk
-echo "Read test: Cache Off"
+echo "Test: Cache Off"
+sudo /sbin/sysctl -w vm.drop_caches=3
 hdparm -W0 $disk
-sudo hdparm -Tt $disk
+ hdparm -Tt $disk
 
 echo "Bad Sectors Check"
 badblocks -v $disk > badsectors.txt
 if [ -s badsectors.txt ]
 then
-	echo "Informing e2fsck of bad sectors"
-	sudo e2fsck -l badsectors.txt $disk
+        echo "Informing e2fsck of bad sectors"
+         e2fsck -l badsectors.txt $disk
 fi
 #end
-sudo umount $disk
-sudo rm -r $workplace
+ umount $disk
+ rm -r /tmp/diskcheck-*
